ddd8d328adc4d941d164a486eb08c435bcb2ee89
ramesh <romanchaudhary01994@gmail.com>
2023-04-28 22:35:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+import './App.css';
+import Timer from './Component/Timer';
+
+function App() {
+  return (
+    <div className="App">
+      <Timer/>
+    </div>
+  );
+}
+
+export default App;
+ 
